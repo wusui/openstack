aa8a036d7232336916e4a176c162df0bf4e09f84
Warren Usui <wusui@redhat.com>
2016-04-22 17:13:26 -0700

    OpenStack Scripts

---
+++ openstack.sh
@@ -0,0 +1,23 @@
+#/bin/bash -fv
+#
+# Install Openstack.
+#     Usage: openstack <openstack-site> <ceph-monitor>
+#
+# This script installs Openstack on one node, and connects it to a ceph
+# cluster on another set of nodes.  It is intended to run from a third
+# node.
+#
+# Assumes a single node Openstack cluster and a single monitor ceph
+# cluster.
+#
+# The execs directory contains scripts to be run on remote sites.
+# The files directory contains files to be copied to remote sites.
+#
+
+source ./copy_func.sh
+source ./fix_conf_file.sh
+openstack_node=${1-magna064}
+ceph_node=${2-magna029}
+./packstack.sh $openstqack_node $ceph_node
+./connectceph.sh $openstqack_node $ceph_node
+./image_create.sh $openstqack_node $ceph_node
